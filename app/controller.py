5eab575594786a33be8716ab6690acd23ef1a439
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-02 21:08:15 +0100

    final version with computer choice too

--- app/controller.py
+++ app/controller.py
@@ -1,12 +1,13 @@
 from app import app
 from flask import render_template, request, redirect
 from app.models.players import Player
-from app.models.games_played import who_wins
+from app.models.games_played import who_wins, add_game
+from app.models.games import Game
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", title = "Home")
+    return render_template("welcome.html", title = "Welcome")
 
 @app.route('/results', methods=['POST'])
 def take_turn():
@@ -16,6 +17,13 @@ def take_turn():
     player_2_choice = request.form['choice_2']
     player_1 =Player(player_1_name,player_1_choice)
     player_2 = Player(player_2_name,player_2_choice)
-    return who_wins(player_1, player_2)
-  
+    result = who_wins(player_1,player_2)
+    add_game(result)
+    return result
+    
+
+@app.route('/home')
+def welcome():
+    return render_template("index.html", title="Home")
+
 
--- app/models/games_played.py
+++ app/models/games_played.py
@@ -1,34 +1,51 @@
 from app.models.players import Player
+from app.models.games import Game
+import random
 
-player_1 = Player("Joe","rock")
-player_2 = Player("Fred","scissors")
+game_result=[]
 
 def who_wins(player_1, player_2):
-    if player_1.choice=="rock":
-        if player_2.choice=="scissors":
-            winner = player_1
-        elif player_2.choice=="paper":
-            winner=player_2
+    if player_2.name=="computer":
+        choice=random.randint(1,3)
+        if choice==1:
+            player_choice="rock"
+        elif choice==2:
+            player_choice="paper"
         else:
-            winner=None
-    elif player_1.choice=="paper":
-        if player_2.choice=="rock":
+            player_choice="scissors"
+    else:
+        player_choice=player_2.choice
+    
+    if player_1.choice=="rock":
+        if player_choice=="scissors":
             winner = player_1
-        elif player_2.choice=="scissors":
+        elif player_choice=="paper":
             winner = player_2
         else:
             winner=None
+    elif player_1.choice=="paper":
+        if player_choice=="rock":
+            winner = player_1
+        elif player_choice=="scissors":
+            winner = player_2.name
+        else:
+            winner=None
     elif player_1.choice=="scissors":
-        if player_2.choice=="paper":
+        if player_choice=="paper":
             winner= player_1
-        elif player_2.choice=="rock":
+        elif player_choice=="rock":
             winner=player_2
         else:
             winner = None
     
     if winner == None:      
-        return "Neither player wins they both chose " + player_1.choice + "."
+        result= f"Neither player won as both players chose {player_1.choice}"
     elif winner.name==player_1.name :
-        return winner.name + " wins as (s)he chose " + winner.choice + " and " + player_2.name + " lost and (s)he chose " + player_2.choice + "."
+        result = f"{player_1.name} wins by playing {player_1.choice} and {player_2.name} loses by playing {player_choice}"
     else:
-        return winner.name + " wins as (s)he chose " + winner.choice + " and " + player_1.name + " lost and (s)he chose " + player_1.choice + "."
+        result= f"{player_2.name} wins by playing {player_choice} and {player_1.name} loses by playing {player_1.choice}"
+    
+    return result
+
+def add_game(result):
+    game_result.append(result)
